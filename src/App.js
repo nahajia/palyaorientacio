0079d58f06db7212f346126a34a79846d8aa8f47
Aniko <nahajia@gmail.com>
2025-10-02 14:07:27 +0200

    alcim

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Pályaorientáció</h1>
+      <h2>Ha pályaválasztás előtt állsz, érdemes
+tisztázni, hogy milyen képességekkel rendelkezel. A képességek erősen befolyásolják, hogy
+pályádon, egy foglalkozás gyakorlásánál mennyire leszel sikeres. Az alábbi kérdőív ebben jelenthet
+segítséget: kitöltésével tájékozódhatsz a képességeidről, melyek jelentik az erősségeidet és melyek
+azok, amelyekben gyengébb vagy.
+</h2>
     </div>
   );
 }
